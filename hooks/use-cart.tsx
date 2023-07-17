0689488069003d40bbb4997f9b03073128a2301b
l-ouiis <72092628+l-ouiis@users.noreply.github.com>
2023-07-17 10:49:17 +0200

    addItem
    Kund kan obegränsat lägga till menyer

--- hooks/use-cart.tsx
+++ hooks/use-cart.tsx
@@ -16,13 +16,6 @@ const useCart = create(
   persist<CartStore>((set, get) => ({
   items: [],
   addItem: (data: Product) => {
-    const currentItems = get().items;
-    const existingItem = currentItems.find((item) => item.id === data.id);
-    
-    if (existingItem) {
-      return toast('Item already in cart.');
-    }
-
     set({ items: [...get().items, data] });
     toast.success('Item added to cart.');
   },
